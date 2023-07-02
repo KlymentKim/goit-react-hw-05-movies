9044d30db48a594b78f80c2b950ece435586faea
Klyment <klim86kr@gmail.com>
2023-07-02 20:51:39 +0300

    fix

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -22,6 +22,22 @@ export const Header = styled.header`
   box-shadow: 0px 15px 10px -15px #111;   
   background-color: #f5f5f5;
 
+  background: linear-gradient(-45deg, #ee7759, #23d547, #23a6d5, #23d5ab);
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
+  
+  @keyframes gradient {
+      0% {
+        background-position: 0% 50%;
+      }
+      50% {
+        background-position: 100% 50%;
+      }
+      100% {
+        background-position: 0% 50%;
+      }
+    }
+
   > nav {
     display: flex;
     gap: 20px
